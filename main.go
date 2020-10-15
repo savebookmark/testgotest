bef21eff5b80184c03c5fa5e94bda7312784cf5b
haho <haho@hohoha.ha>
2020-10-15 22:42:02 +0900

    bitree

--- main.go
+++ main.go
@@ -80,8 +80,31 @@ func main() {
 	//hahahah branch m
 	tree.DFS2()
 
+	bitree()
 }
-func aaabbbba(aaaaa int) string {
-	a := "hi"
-	return a
+func bitree() {
+	tree := dataStruct.NewBinaryTree(5)
+	tree.Root.AddNode(3)
+	tree.Root.AddNode(2)
+	tree.Root.AddNode(4)
+	tree.Root.AddNode(8)
+	tree.Root.AddNode(7)
+	tree.Root.AddNode(6)
+	tree.Root.AddNode(10)
+	tree.Root.AddNode(9)
+
+	tree.Print()
+	fmt.Println()
+
+	if found, cnt := tree.Search(6); found {
+		fmt.Println("found 6 cnt:", cnt)
+	} else {
+		fmt.Println("not found 6 cnt:", cnt)
+	}
+
+	if found, cnt := tree.Search(11); found {
+		fmt.Println("found 11 cnt:", cnt)
+	} else {
+		fmt.Println("not found 11 cnt:", cnt)
+	}
 }
